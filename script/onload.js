d3cbcf56d53a2098b5c0176e209c30776b7b2c95
DIARY-PC\程达瑞 <383001400@qq.com>
2018-03-25 01:11:14 +0800

    animation linkarea

--- script/onload.js
+++ script/onload.js
@@ -1,5 +1,14 @@
-window.onload=function () {
+window.onload = function () {
 	
 	handleTimer();
 	handleCalendar();
+}
+
+window.onscroll = function () {
+	
+	var scrollTop = document.documentElement.scrollTop;
+	if(scrollTop >= 200){
+		document.getElementById('calendar').classList.add('moveDown')
+		document.getElementById('linkArea').classList.add('moveDown')
+	}
 }
